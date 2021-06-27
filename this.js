249204318e2b59e56ae08a751d7fd5aa229648e7
Anshika <86038330+Anshika68@users.noreply.github.com>
2021-06-27 14:35:25 +0530

    Create this.js

---
+++ this.js
@@ -0,0 +1,7 @@
+function gfg(imgs) {
+    var expandImg = document.getElementById("expand");
+    var imgText = document.getElementById("geeks");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+}
